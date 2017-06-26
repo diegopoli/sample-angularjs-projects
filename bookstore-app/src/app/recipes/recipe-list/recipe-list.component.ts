a63c9d700f866ff9063eca7c27b37bba6840ff36
Diego Poli <diego.apr@gmail.com>
2017-06-26 15:34:38 -0700

    Outputting the recipe list on the screen.

--- bookstore-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ bookstore-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Summery Zucchini-Basil Soup',
+      'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2017/04/20170426-zucchini-pesto-soup-3-thumb-1500xauto-437445.jpg'),
+    new Recipe('Fava Bean and Carrot Salad With Ricotta',
+      'This is simply a test', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+  ];
 
   constructor() { }
 

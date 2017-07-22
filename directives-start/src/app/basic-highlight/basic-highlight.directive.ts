a246e1844faff952ce81f9cb633e4fcc9471e565
Diego Poli <diego.apr@gmail.com>
2017-07-21 17:49:23 -0700

    Created a better attribute directive by using Renderer and ElementRef.

--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -4,10 +4,13 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
   selector: '[appBasicHighlight]' /* This selector shall be used as an attribute just because of the brackets [...]. */
 })
 export class BasicHighlightDirective implements OnInit {
+
   constructor(private elementRef: ElementRef) {
   }
 
   ngOnInit() {
+  	// This directive accesses the DOM directly.
     this.elementRef.nativeElement.style.backgroundColor = 'green';
+    this.elementRef.nativeElement.style.color = 'white';
   }
 }
